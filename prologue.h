2c7a86ed6b1aef4a042a12a9374b5fb27818f1e2
Erik Edlund <erik.o.edlund@gmail.com>
2011-12-01 07:49:42 +0100

    Add a prologue file for libcutie.

---
+++ prologue.h
@@ -0,0 +1,59 @@
+/* cutie - C UTilitIEs
+ * Copyright (c) 2011, Erik Edlund <erik.o.edlund@gmail.com>
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of Erik Edlund, nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* libcutie amalgamation - https://github.com/edlund/libcutie
+ * 
+ * This file is a part of an amalgamation of the source and
+ * header files from libcutie. Two files are required to use
+ * an amalgamation of libcutie in another project, namely
+ * `cutie.h` and `cutie.c`.
+ * 
+ * - cutie.h is an amalgamation of all public header files.
+ * - cutie.c is an amalgamation of all header and source
+ *   files. It makes it possible to compile libcutie as a
+ *   single translation unit.
+ * 
+ * To use an amalgamation to integrate libcutie with another
+ * project, simply make sure that
+ * 
+ * 1) cutie.h is available on one of your include paths and
+ * 2) cutie.c is compiled and linked with the rest of your
+ *    source files.
+ * 
+ * For more information, please refer to the README-file for
+ * libcutie.
+ * 
+ * This file was automatically generated, so think twice
+ * before making changes to it. Most likely, it is easier
+ * to clone the project, make changes to the source and then
+ * create a new amalgamation.
+ */
+
+#define CUTIE_AMALGAMATION 1
+
